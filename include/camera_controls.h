d917dd39307f3e759eeeeadb403a896584e0c63d
Xander Azuaje <charon-mortis@pm.me>
2025-01-25 17:42:42 +0100

    created rotation control

--- include/camera_controls.h
+++ include/camera_controls.h
@@ -6,17 +6,14 @@
 /*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:08:45 by xazuaje-          #+#    #+#             */
-/*   Updated: 2025/01/24 14:32:47 by xazuaje-         ###   ########.fr       */
+/*   Updated: 2025/01/25 17:42:23 by xazuaje-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-//
-// Created by xander on 1/24/25.
-//
 
 #ifndef CAMERA_CONTROLS_H
 #define CAMERA_CONTROLS_H
 #define MOVE_SPEED 0.5f
+#define ROTATION_SPEED 5.0f
 #include "types.h"
 #include "stdlib.h"
 #include "rays.h"
--- src/controls/camera_controls.c
+++ src/controls/camera_controls.c
@@ -1,21 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   camera_controls.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xazuaje- <xazuaje-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 14:08:17 by xazuaje-          #+#    #+#             */
+/*   Updated: 2025/01/25 17:38:20 by xazuaje-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <camera_controls.h>
+#include <quaternion_operations.h>
+
+void rotate_camera(t_quat *rotation, double angle, t_vec3 axis)
+{
+    double half_angle;
+    double sin_half_angle;
+
+    half_angle = angle / 2.0;
+    sin_half_angle = sin(half_angle * ( M_PI / 180));
+    *rotation = multiply_quaternion(*rotation, (t_quat){
+        .w = cos(half_angle),
+        .x = axis.x * sin_half_angle,
+        .y = axis.y * sin_half_angle,
+        .z = axis.z * sin_half_angle,
+    });
+}
 
 void move_camera(t_scene *scene, mlx_t *mlx)
 {
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		scene->cameras[0].coords.z += MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		scene->cameras[0].coords.z -= MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		scene->cameras[0].coords.x += MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		scene->cameras[0].coords.x -= MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_J))
-		scene->cameras[0].coords.y += MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_K))
-		scene->cameras[0].coords.y -= MOVE_SPEED;
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
+    if (mlx_is_key_down(mlx, MLX_KEY_UP))
+        scene->cameras[0].coords.z += MOVE_SPEED;
+    if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+        scene->cameras[0].coords.z -= MOVE_SPEED;
+    if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+        scene->cameras[0].coords.x += MOVE_SPEED;
+    if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+        scene->cameras[0].coords.x -= MOVE_SPEED;
+    if (mlx_is_key_down(mlx, MLX_KEY_J))
+        scene->cameras[0].coords.y += MOVE_SPEED;
+    if (mlx_is_key_down(mlx, MLX_KEY_K))
+        scene->cameras[0].coords.y -= MOVE_SPEED;
+    if (mlx_is_key_down(mlx, MLX_KEY_KP_8))
+        rotate_camera(&scene->cameras[0].rotation, -ROTATION_SPEED, (t_vec3){1.0, 0.0, 0.0}); // Rotar hacia arriba
+    if (mlx_is_key_down(mlx, MLX_KEY_KP_2))
+        rotate_camera(&scene->cameras[0].rotation, ROTATION_SPEED, (t_vec3){1.0, 0.0, 0.0}); // Rotar hacia abajo
+    if (mlx_is_key_down(mlx, MLX_KEY_KP_4))
+        rotate_camera(&scene->cameras[0].rotation, ROTATION_SPEED, (t_vec3){0.0, 1.0, 0.0}); // Rotar hacia la izquierda
+    if (mlx_is_key_down(mlx, MLX_KEY_KP_6))
+        rotate_camera(&scene->cameras[0].rotation, -ROTATION_SPEED, (t_vec3){0.0, 1.0, 0.0}); // Rotar hacia la derecha
+    if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+        mlx_close_window(mlx);
 }
 
 void camera_controls(void *param)
