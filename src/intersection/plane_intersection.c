661f4c4504e0f466afde43927ee2d9c001a99848
Ines Sainz Ramon <isainz-r@c3r7s6.42madrid.com>
2025-01-28 10:59:44 +0100

    sphere and plane correct now

--- src/intersection/plane_intersection.c
+++ src/intersection/plane_intersection.c
@@ -17,14 +17,15 @@ short plane_intersection(t_ray ray, t_scene_element *elem, int bounce)
     t_plane *p;
     (void)bounce;
     p = &elem->plane;
-    t_vec3  origin_less_pplane = substract_vec3(ray.direction, p->coords);
+
+    t_vec3  origin_less_pplane = substract_vec3(ray.position, p->coords);
     float   origin_less_plane_o_normal = dot_product_vec3(origin_less_pplane, p->rotate_vec);
     float   dir_o_normal = dot_product_vec3(ray.direction, p->rotate_vec);
 
     if (dir_o_normal == 0)
         return (0);
 
-    float   t = (-origin_less_plane_o_normal / dir_o_normal);
+    float   t = ((-1 * origin_less_plane_o_normal) / dir_o_normal);
 
     t_vec3 p_intersection = add_vec3(ray.position, (t_vec3){t * ray.direction.x, t * ray.direction.y, t * ray.direction.z});
     (void)p_intersection;
--- src/intersection/sphere_intersection.c
+++ src/intersection/sphere_intersection.c
@@ -12,41 +12,35 @@
 
 #include <intersection.h>
 
+/*||p
+w = ray_direction - sphere_center
+a = ray_direction * ray_direction
+b = 2 * (w * ray_direction)
+c = w * w - s_Radius²
+discriminant = b² - 4ac
+*/
 short sphere_intersection(t_ray ray, t_scene_element *elem, int bounce)
 {
-	t_sphere *s;
-	t_vec3 oc;  // Vector from sphere center to ray origin
-	double a, b, c;
-	double discriminant;
-	double t1, t2;
-
+	t_sphere	*s;
 	(void)bounce;
 	s = &elem->sphere;
-	
-	// Calculate vector from sphere center to ray origin
-	oc = substract_vec3(ray.position, s->coords);
-	
-	// Calculate quadratic equation coefficients
-	a = dot_product_vec3(ray.normalized, ray.normalized);  // Should be 1.0 if normalized
-	b = 2.0 * dot_product_vec3(ray.normalized, oc);
-	c = dot_product_vec3(oc, oc) - (s->radius * s->radius);
-	
-	// Calculate discriminant
-	discriminant = b * b - 4 * a * c;
-	
+
+	t_vec3	w = substract_vec3(ray.position, s->coords);
+	float	a = dot_product_vec3(ray.direction, ray.direction);
+	float	b = 2 * dot_product_vec3(w, ray.direction);
+	float	c = (dot_product_vec3(w, w)) - (s->radius * s->radius);
+	float	discriminant = (b * b) - (4 * (a * c));
+
 	if (discriminant < 0)
 		return (0);
-		
-	// Calculate intersection points
-	t1 = (-b - sqrt(discriminant)) / (2.0 * a);
-	t2 = (-b + sqrt(discriminant)) / (2.0 * a);
-	
-	// Check if sphere is in front of the ray
-	if (t1 > 0.001 || t2 > 0.001)  // Small epsilon to avoid self-intersection
-	{
-		ray.rgba = s->rgba;
-		return (1);
-	}
-	
-	return (0);
+
+	float	t1 = ((- b) - (sqrt(discriminant))) / (2 * a);
+	float	t2 = ((-b) + (sqrt(discriminant))) / (2 * a);
+
+	t_vec3	p1 = add_vec3(ray.position, (t_vec3){t1 * ray.direction.x, t1 * ray.direction.y, t1 * ray.direction.z});
+	t_vec3	p2 = add_vec3(ray.position, (t_vec3){t2 * ray.direction.x, t2 * ray.direction.y, t2 * ray.direction.z});
+
+	(void)p1;
+	(void)p2;
+	return (1);
 }
